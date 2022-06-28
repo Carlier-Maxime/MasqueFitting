9c72ad3106b92670999d06e4ff28eb26104aa83c
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-06-28 16:02:16 +0200

    fix INSTALL.py

--- INSTALL.py
+++ INSTALL.py
@@ -6,5 +6,5 @@ os.system('git clone https://gitlab.com/libeigen/eigen.git')
 os.system('make')
 os.chdir('../../../..')
 
-if bool(input("Veux tu supprimer le fichier d'installation ? (0|1) : ")):
+if bool(int(input("Veux tu supprimer le fichier d'installation ? (0|1) : "))):
     os.remove('INSTALL.py')
