db25a8ce8314ba001d28bf844b8c9128de842f3a
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-06-28 10:10:45 +0200

    add possibility to choice delete INSTALL.py after install

--- INSTALL.py
+++ INSTALL.py
@@ -4,3 +4,7 @@ os.system('pip install -r requirements.txt')
 os.chdir('flame-fitting/sbody/alignment/mesh_distance')
 os.system('git clone https://gitlab.com/libeigen/eigen.git')
 os.system('make')
+os.chdir('../../../')
+
+if bool(input('Do you want to delete the installation file ? (0|1) : ')):
+    os.remove('INSTALL.py')
