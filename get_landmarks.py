5f4fc568e98e11152c32f9ed0ea06c2ed5808fc9
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-07-04 14:36:10 +0200

    screenshot view for use facial landmark detection

--- get_landmarks.py
+++ get_landmarks.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import DirectionalLight
@@ -17,6 +18,13 @@ class MyApp(ShowBase):
         base.disableMouse()
         base.setBackgroundColor(1, 1, 1)
         base.camera.setPosHpr(0, 0, 300, 0, -84, 0)
+        taskMgr.doMethodLater(1, self.screenshotTask, 'screenshot')
+
+    def screenshotTask(self, task):
+        if not os.path.isdir("tmp"):
+            os.mkdir("tmp")
+        base.screenshot("tmp/screen.png", False)
+        return task.done
 
 
 if __name__ == '__main__':
